dcd6376f677d400f2f05a61a2d63051f6446eabd
jayce <jayce.baxter@proton.me>
2025-01-31 12:22:35 -0500

    improved clairity in output messages

--- projectileCalculator/main.java
+++ projectileCalculator/main.java
@@ -5,24 +5,24 @@ public class main{
     public static void main(String [] args){
         final double gravForce = 9.8;
         Scanner userInput = new Scanner(System.in);
-        System.out.print("Please enter the initial speed of the projectile: ");
+        System.out.print("Please enter the initial speed of the projectile in meters/second: ");
     	  
         // Initial speed will be in meters per second  
         double initialSpeed = userInput.nextDouble();
         
         // Initial angle is in degrees
-        System.out.print("Please enter the initial angle of the projectile: ");
+        System.out.print("Please enter the initial angle of the projectile in degrees: ");
         double initialAngle = userInput.nextDouble();
         double angleCOS = Math.cos(initialAngle);
         double angleSIN = Math.sin(initialAngle);
 
         // Flight time is in seconds
-        System.out.print("Please enter the flight time of the projectile: ");
+        System.out.print("Please enter the flight time of the projectile in seconds: ");
         double flightTime = userInput.nextDouble();
 
         double xCoordinate = (initialSpeed * angleCOS) * flightTime;
         double yCoordinate = (initialSpeed * angleSIN) * flightTime - 0.5 * gravForce * flightTime * flightTime;
 
-        System.out.println("With a flight time of " + flightTime + ", the x coordinate will be " + xCoordinate + "and the y coordinate will be " + yCoordinate);
+        System.out.println("With a flight time of " + flightTime + ", the x coordinate will be " + xCoordinate + " and the y coordinate will be " + yCoordinate);
     }
 }
